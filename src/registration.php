7aee7285fb6758c17c45d553d6eaf442979b37b7
cromero <cromero@digitalorigin.com>
2018-01-16 17:21:07 +0100

    updated module

--- src/registration.php
+++ src/registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Magento_SamplePaymentGateway',
+    'DigitalOrigin_Pmt',
     __DIR__
 );
