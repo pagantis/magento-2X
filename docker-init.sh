2e553d8574243dc2f7d4519169efd968fbe82c9c
cromero <cromero@digitalorigin.com>
2018-01-18 16:50:00 +0100

    updated docker init + di.xml

--- docker-init.sh
+++ docker-init.sh
@@ -14,12 +14,15 @@ echo 'Install Magento'
 docker-compose exec --user=root magento20 chown -R magento2:magento2 /var/www/ && \
 docker-compose exec --user=root magento20 chown -R magento2:magento2 /home/magento2/ && \
 docker-compose exec --user=magento2 magento20 /home/magento2/scripts/m2init magento:install --no-interaction --magento-host=magento20 && \
-
+echo 'Install DigitalOrigin_Pmt'
 docker-compose exec --user=magento2 magento20 mkdir -p /var/www/magento2/app/code/DigitalOrigin && \
 docker-compose exec --user=magento2 magento20 ln -s /paylater /var/www/magento2/app/code/DigitalOrigin/Pmt && \
-
 docker-compose exec --user=magento2 magento20 php /var/www/magento2/bin/magento module:enable DigitalOrigin_Pmt && \
+echo 'Install SampleData + Compile DI + cache:flush'
 docker-compose exec --user=magento2 magento20 php /var/www/magento2/bin/magento sampledata:deploy && \
 docker-compose exec --user=magento2 magento20 php /var/www/magento2/bin/magento setup:upgrade && \
-docker-compose exec --user=magento2 magento20 php /var/www/magento2/bin/magento setup:di:compile && \
+
+# docker-compose exec --user=magento2 magento20 php /var/www/magento2/bin/magento setup:di:compile && \
+
 docker-compose exec --user=magento2 magento20 php /var/www/magento2/bin/magento cache:flush
+echo 'Build of Magento2 enviroment complete: http://magento20:1748'
