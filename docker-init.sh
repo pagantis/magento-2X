8e1d71db07671178d4f628902fb71c61582ac181
cromero <cromero@digitalorigin.com>
2018-02-28 13:42:41 +0100

    Working version of magento 2.2 with docker and PMT installed

--- docker-init.sh
+++ docker-init.sh
@@ -11,12 +11,11 @@ docker-compose up -d
 sleep 10
 
 echo 'Install Magento'
-docker-compose exec magento20 install-magento
+docker-compose exec magento2 install-magento
 echo 'Install DigitalOrigin_Pmt'
-docker-compose exec magento20 mkdir -p /var/www/html/app/code/DigitalOrigin && \
-docker-compose exec magento20 ln -s /var/www/paylater /var/www/html/app/code/DigitalOrigin/Pmt && \
-docker-compose exec magento20 php /var/www/html/bin/magento module:enable DigitalOrigin_Pmt && \
+docker-compose exec --user=www-data magento2 mkdir -p /var/www/html/app/code/DigitalOrigin && \
+docker-compose exec --user=www-data magento2 ln -s /var/www/paylater /var/www/html/app/code/DigitalOrigin/Pmt && \
+docker-compose exec --user=www-data magento2 php /var/www/html/bin/magento module:enable DigitalOrigin_Pmt && \
 echo 'Sample Data + DI + SetupUpgrade + Clear Cache'
-docker-compose exec magento20 install-sampledata
-docker-compose exec magento20 php /var/www/magento2/bin/magento cache:flush
-echo 'Build of Magento2 enviroment complete: http://magento20:8086'
+docker-compose exec magento2 install-sampledata
+echo 'Build of Magento2 enviroment complete: http://magento2:8086'
