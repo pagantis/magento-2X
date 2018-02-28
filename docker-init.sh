0f5900f48894a457f71e1fcc53d36f7bdf946263
cromero <cromero@digitalorigin.com>
2018-02-28 14:04:16 +0100

    Updated docker-init file

--- docker-init.sh
+++ docker-init.sh
@@ -18,4 +18,5 @@ docker-compose exec --user=www-data magento2 ln -s /var/www/paylater /var/www/ht
 docker-compose exec --user=www-data magento2 php /var/www/html/bin/magento module:enable DigitalOrigin_Pmt && \
 echo 'Sample Data + DI + SetupUpgrade + Clear Cache'
 docker-compose exec magento2 install-sampledata
-echo 'Build of Magento2 enviroment complete: http://magento2:8086'
+docker-compose exec -u www-data magento2 /var/www/html/bin/magento cron:run
+echo 'Build of Magento2 enviroment complete: http://magento2'
