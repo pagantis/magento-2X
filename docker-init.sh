a5f3eee670cd11ed19308359a1be5381cef95283
cromero <cromero@digitalorigin.com>
2018-03-01 18:58:51 +0100

    Updated based on adyen module

--- docker-init.sh
+++ docker-init.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
-
-echo 'Composer install'
-composer install
-echo 'Npm install + grunt'
-npm install
-grunt default
 echo 'Build docker images'
 docker-compose down
-docker-compose up -d
+docker-compose up -d --build
 sleep 10
 
 echo 'Install Magento'
 docker-compose exec magento2 install-magento
 echo 'Install DigitalOrigin_Pmt'
-docker-compose exec --user=www-data magento2 mkdir -p /var/www/html/app/code/DigitalOrigin && \
-docker-compose exec --user=www-data magento2 ln -s /var/www/paylater /var/www/html/app/code/DigitalOrigin/Pmt && \
-docker-compose exec --user=www-data magento2 php /var/www/html/bin/magento module:enable DigitalOrigin_Pmt && \
+#docker-compose exec --user=www-data magento2 mkdir -p /var/www/html/app/code/DigitalOrigin && \
+#docker-compose exec --user=www-data magento2 ln -s /var/www/paylater /var/www/html/app/code/DigitalOrigin/Pmt && \
+#docker-compose exec --user=www-data magento2 php /var/www/html/bin/magento module:enable DigitalOrigin_Pmt && \
 echo 'Sample Data + DI + SetupUpgrade + Clear Cache'
 docker-compose exec magento2 install-sampledata
 docker-compose exec -u www-data magento2 /var/www/html/bin/magento cron:run
