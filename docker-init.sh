61e23a5eb892fba7588dbeba6adf540a87a3d003
cromero <cromero@digitalorigin.com>
2018-03-05 09:11:32 +0100

    updated docker installation. Composer install in local paylater dir

--- docker-init.sh
+++ docker-init.sh
@@ -11,13 +11,13 @@ docker-compose up -d selenium
 sleep 10
 
 echo 'Install Magento'
-docker-compose exec magento2-${ENVIROMENT} composer install
 docker-compose exec magento2-${ENVIROMENT} install-magento
-echo 'Install DigitalOrigin_Pmt'
-#docker-compose exec --user=www-data magento2-${ENVIROMENT} mkdir -p /var/www/html/app/code/DigitalOrigin && \
-#docker-compose exec --user=www-data magento2-${ENVIROMENT} ln -s /var/www/paylater /var/www/html/app/code/DigitalOrigin/Pmt && \
-#docker-compose exec --user=www-data magento2-${ENVIROMENT} php /var/www/html/bin/magento module:enable DigitalOrigin_Pmt && \
+#echo 'Install DigitalOrigin_Pmt'
+#docker-compose exec -u www-data magento2-${ENVIROMENT} mkdir -p /var/www/html/app/code/DigitalOrigin && \
+#docker-compose exec -u www-data magento2-${ENVIROMENT} ln -s /var/www/paylater /var/www/html/app/code/DigitalOrigin/Pmt && \
+#docker-compose exec -u www-data magento2-${ENVIROMENT} php /var/www/html/bin/magento module:enable DigitalOrigin_Pmt && \
 echo 'Sample Data + DI + SetupUpgrade + Clear Cache'
 docker-compose exec magento2-${ENVIROMENT} install-sampledata
 docker-compose exec -u www-data magento2-${ENVIROMENT} /var/www/html/bin/magento cron:run
-echo 'Build of Magento2 enviroment complete: http://magento2'
+docker-compose exec -u www-data magento2-${ENVIROMENT} composer install -d /var/www/paylater
+echo 'Build of Magento2 enviroment complete: http://magento2.docker:8086'
