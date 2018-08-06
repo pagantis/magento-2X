d64976cd1783f59e28cd618bd44e75e8b6281d67
pgarces <gspablo@gmail.com>
2018-08-06 08:11:07 +0200

    Iframe

--- Block/Payment/Iframe.php
+++ Block/Payment/Iframe.php
@@ -3,141 +3,125 @@
 namespace DigitalOrigin\Pmt\Block\Payment;
 
 use Magento\Backend\Block\Template\Context;
-use Magento\Catalog\Model\Category;
-use Magento\Catalog\Model\Product;
-use Magento\Framework\Registry;
 use Magento\Framework\View\Element\Template;
+use Magento\Checkout\Model\Session;
+use Magento\Framework\Session\SessionManagerInterface;
 
-/**
- * Class Iframe
- * @package DigitalOrigin\Pmt\Block\Product
- */
 class Iframe extends Template
 {
-    const PROMOTIONS_CATEGORY = 'paylater-promotion-product';
-
     /**
-     * @var bool
+     * @var string
      */
-    protected $enabled;
+    public $orderUrl;
 
     /**
      * @var string
      */
-    protected $publicKey;
-
-    /**
-     * @var int
-     */
-    protected $simulatorType;
+    public $checkoutUrl;
 
     /**
      * @var string
      */
-    protected $promotionProductExtra;
+    public $email;
 
     /**
-     * @var Product
+     * @var string
      */
-    protected $product;
+    public $session;
 
     /**
-     * @var Registry
+     * @var ObjectManager $objectManager
      */
-    protected $registry;
+    protected $objectManager;
 
     /**
-     * Simulator constructor.
+     * Iframe constructor.
      *
-     * @param Context  $context
-     * @param Registry $registry
-     * @param array    $data
+     * @param Context                 $context
+     * @param Session                 $session
+     * @param SessionManagerInterface $coreSession
+     * @param array                   $data
      */
     public function __construct(
         Context $context,
-        Registry $registry,
+        Session $session,
+        SessionManagerInterface $coreSession,
         array $data = []
     ) {
         parent::__construct($context, $data);
+        $this->session = $session;
+        //$this->objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        /*$this->coreSession = $coreSession;
 
-        $this->registry = $registry;
-        /** @var \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig */
-        $scopeConfig = $this->_scopeConfig;
-        $config = $scopeConfig->getValue('payment/paylater');
-        $isProduction = $config['sandbox'];
-        $this->publicKey = $isProduction ? $config['prod_public_key'] : $config['test_public_key'];
-        $this->simulatorType = $config['product_simulator'];
-        $this->enabled = $config['active'];
-        $this->promotionProductExtra = $config['promotion_extra'];
-    }
-
-    /**
-     * @return Product
-     */
-    protected function getProduct()
-    {
-        if (is_null($this->product)) {
-            $this->product = $this->registry->registry('product');
+        $this->coreSession->start();
+        $info = explode("&", $this->coreSession->getMessage());
+        foreach ($info as $infoValue) {
+            $infoParam = explode("=", $infoValue);
+            $infoVar = $infoParam[0];
+            $this->$infoVar = $infoParam[1];
         }
-
-        return $this->product;
-    }
-
-    /**
-     * @return bool
-     */
-    public function isEnabled()
-    {
-        return $this->enabled;
+        var_dump($this->getEmail());
+        die('FIIIN');*/
     }
 
     /**
      * @return string
      */
-    public function getPublicKey()
+    public function getOrderUrl()
     {
-        return $this->publicKey;
+        return $this->orderUrl;
     }
 
     /**
-     * @return int
+     * @param string $orderUrl
+     *
+     * @return Iframe
      */
-    public function getSimulatorType()
+    public function setOrderUrl($orderUrl)
     {
-        return $this->simulatorType;
+        $this->orderUrl = $orderUrl;
+
+        return $this;
     }
 
     /**
      * @return string
      */
-    public function getPromotionProductExtra()
+    public function getCheckoutUrl()
     {
-        return $this->promotionProductExtra;
+        return $this->checkoutUrl;
     }
 
     /**
-     * @return bool
+     * @param string $checkoutUrl
+     *
+     * @return Iframe
      */
-    public function isProductInPromotion()
+    public function setCheckoutUrl($checkoutUrl)
     {
-        try {
-            /** @var Category $category */
-            $category = $this->getProduct()->getCategoryCollection()->addFieldToFilter(
-                'name',
-                array('eq' => self::PROMOTIONS_CATEGORY)
-            )->getFirstItem();
-        } catch (\Exception $exception) {
-            return false;
-        }
+        $this->checkoutUrl = $checkoutUrl;
 
-        return $category->getName() === self::PROMOTIONS_CATEGORY ? 1 : 0;
+        return $this;
     }
 
     /**
-     * @return float
+     * @return string
      */
-    public function getFinalPrice()
+    public function getEmail()
     {
-        return $this->getProduct()->getFinalPrice();
+        return $this->email;
     }
+
+    /**
+     * @param string $email
+     *
+     * @return Iframe
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
 }
---
+++ Controller/Payment/Iframe.php
@@ -0,0 +1,61 @@
+<?php
+namespace DigitalOrigin\Pmt\Controller\Payment;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\View\Element\BlockInterface;
+
+class Iframe extends Action
+{
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $_pageFactory;
+
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $pageFactory,
+        \DigitalOrigin\Pmt\Helper\Config $pmtconfig
+    ) {
+        $this->_pageFactory = $pageFactory;
+        $this->config = $pmtconfig->getConfig();
+        return parent::__construct($context);
+    }
+
+    /**
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface|\Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        try {
+            $resultPage = $this->_pageFactory->create();
+            $orderId = $this->getRequest()->getParam('orderId');
+            if ($orderId == '') {
+                throw new \Exception('Empty orderId');
+            }
+
+            if ($this->config['public_key'] == '' || $this->config['secret_key'] == '') {
+                throw new \Exception('Public and Secret Key not found');
+            }
+
+            $orderClient = new \PagaMasTarde\OrdersApiClient\Client(
+                $this->config['public_key'],
+                $this->config['secret_key']
+            );
+
+            $order = $orderClient->getOrder($orderId);
+
+            /** @var \DigitalOrigin\Pmt\Block\Payment\Iframe $block */
+            $block = $resultPage->getLayout()->getBlock('paylater_payment_iframe');
+            $block
+                ->setEmail($order->getUser()->getEmail())
+                ->setOrderUrl($order->getActionUrls()->getForm())
+                ->setCheckoutUrl($order->getConfiguration()->getUrls()->getCancel())
+            ;
+
+            return $resultPage;
+        } catch (\Exception $exception) {
+            $this->logger->info("[ERROR_4]".$exception->getMessage());
+            die($exception->getMessage());
+        }
+    }
+}
