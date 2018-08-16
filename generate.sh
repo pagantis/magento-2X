04a2062fc1a88727700490371bc0ab4666fdefb7
pgarces <gspablo@gmail.com>
2018-08-16 15:36:24 +0200

    Travis + PhpUnit

--- generate.sh
+++ generate.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Prepare environment and build package
-./docker-init.sh dev
+./docker-init.sh test
 
 # Time to boot and install magento
-sleep 660
-set -b
+#sleep 660
+set -e
 
 # Run test
-vendor/bin/phpunit --group magento-basic
-vendor/bin/phpunit --group magento-install
-vendor/bin/phpunit --group magento-buy-unregistered
-vendor/bin/phpunit --group magento-register
-vendor/bin/phpunit --group magento-buy-registered
+docker-compose exec magento2-test vendor/bin/phpunit --group magento-basic
+docker-compose exec magento2-test vendor/bin/phpunit --group magento-install
+docker-compose exec magento2-test vendor/bin/phpunit --group magento-buy-unregistered
+docker-compose exec magento2-test vendor/bin/phpunit --group magento-register
+docker-compose exec magento2-test vendor/bin/phpunit --group magento-buy-registered
