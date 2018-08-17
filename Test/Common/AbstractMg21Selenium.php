600ab8f454816e01e7547fb2504b2445abcb4d6b
pgarces <gspablo@gmail.com>
2018-08-17 14:23:56 +0200

    Sleep after product simulator

--- Test/Common/AbstractMg21Selenium.php
+++ Test/Common/AbstractMg21Selenium.php
@@ -185,7 +185,7 @@ abstract class AbstractMg21Selenium extends PaylaterMagentoTest
         $simulatorElement = $this->findByClass('PmtSimulator');
         $currentSimulatorPrice = $simulatorElement->getAttribute('data-pmt-amount');
         $this->configureProduct(self::PRODUCT_QTY_AFTER);
-        sleep(3);
+        sleep(5);
         $simulatorElement = $this->findByClass('PmtSimulator');
         $newPrice = $simulatorElement->getAttribute('data-pmt-amount');
         $newSimulatorPrice = $currentSimulatorPrice * self::PRODUCT_QTY_AFTER;
