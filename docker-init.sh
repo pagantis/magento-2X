866ffb3967e788457f81961721eee098dd60fe44
pgarces <gspablo@gmail.com>
2018-09-12 08:54:35 +0200

    Rewriting PORT&TRAVIS vars

--- docker-init.sh
+++ docker-init.sh
@@ -11,14 +11,12 @@ docker-compose exec magento2-${ENVIROMENT} install-magento
 echo 'Install DigitalOrigin_Pmt'
 if [ $1 == 'dev' ]
 then
-    PORT='8086'
     docker-compose exec -u www-data magento2-${ENVIROMENT} mkdir -p /var/www/html/app/code/DigitalOrigin && \
     docker-compose exec -u www-data magento2-${ENVIROMENT} ln -s /var/www/paylater /var/www/html/app/code/DigitalOrigin/Pmt && \
     docker-compose exec -u www-data magento2-${ENVIROMENT} php /var/www/html/bin/magento module:enable DigitalOrigin_Pmt && \
     docker-compose exec magento2-${ENVIROMENT} chown -R www-data. /var/www/paylater
     docker-compose exec -u www-data magento2-${ENVIROMENT} composer install -d /var/www/paylater
 else
-    PORT='8085'
     version=$(git describe --all HEAD)
     versionParsed=$(sed  -e 's/heads\///' -e 's/-.*//' <<< $version)
     package=$versionParsed'.x-dev'
@@ -27,7 +25,7 @@ else
         package='dev-master'
     fi
     echo "Esta es la rama del pull request" ${TRAVIS_PULL_REQUEST_BRANCH}
-    if [ ${TRAVIS_PULL_REQUEST_BRANCH} != '' ]
+    if [ ! -z "$TRAVIS_PULL_REQUEST_BRANCH" ]
     then
         package=${TRAVIS_PULL_REQUEST_BRANCH}'.x-dev'
     fi
@@ -40,5 +38,7 @@ fi
 echo 'Sample Data + DI + SetupUpgrade + Clear Cache'
 docker-compose exec magento2-${ENVIROMENT} install-sampledata
 docker-compose exec -u www-data magento2-${ENVIROMENT} /var/www/html/bin/magento cron:run
-docker container port magento2-${ENVIROMENT}
+
+containerPort=$(docker container port magento2${ENVIROMENT})
+PORT=$(sed  -e 's/.*://' <<< $containerPort)
 echo 'Build of Magento2 complete: http://magento2-'${ENVIROMENT}'.docker:'${PORT}
