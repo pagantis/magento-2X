b6411eb6489b1ee0cec3c4f4378154a6fab702e4
rmarinleal <raul.marin.leal@gmail.com>
2019-01-30 10:22:42 +0100

    fixing developer environment

--- docker-init.sh
+++ docker-init.sh
@@ -9,23 +9,32 @@ then
     docker-compose up -d selenium
 fi
 docker-compose up -d --build magento2-${ENVIROMENT}
-
-sleep 15
-
+sleep 5
 docker-compose exec magento2-${ENVIROMENT} docker-php-ext-install bcmath
 
 echo 'Install Magento'
 docker-compose exec magento2-${ENVIROMENT} install-magento
+
+echo 'Sample Data + DI + SetupUpgrade + Clear Cache'
+docker-compose exec -u www-data magento2-${ENVIROMENT} composer config http-basic.repo.magento.com \
+    5310458a34d580de1700dfe826ff19a1 \
+    255059b03eb9d30604d5ef52fca7465d
+echo 'Running: sampledata:deploy'
+docker-compose exec -u www-data magento2-${ENVIROMENT} php /var/www/html/bin/magento sampledata:deploy
+echo 'Running: setup:upgrade'
+docker-compose exec -u www-data magento2-${ENVIROMENT} php /var/www/html/bin/magento setup:upgrade
+
 echo 'Install DigitalOrigin_Pmt'
 if [ $1 != 'test' ]
 then
-    docker-compose exec -u www-data magento2-${ENVIROMENT} mkdir -p /var/www/html/app/code/DigitalOrigin
-    docker-compose exec -u www-data magento2-${ENVIROMENT} ln -s /var/www/paylater /var/www/html/app/code/DigitalOrigin/Pmt
+#    docker-compose exec -u www-data magento2-${ENVIROMENT} mkdir -p /var/www/html/app/code/DigitalOrigin
+#    docker-compose exec -u www-data magento2-${ENVIROMENT} ln -s /var/www/paylater /var/www/html/app/code/DigitalOrigin/Pmt
     docker-compose exec -u www-data magento2-${ENVIROMENT} php /var/www/html/bin/magento \
         module:enable DigitalOrigin_Pmt --clear-static-content
-    docker-compose exec magento2-${ENVIROMENT} chown -R www-data. /var/www/paylater
-    docker-compose exec -u www-data magento2-${ENVIROMENT} composer install -d /var/www/paylater
+#    docker-compose exec magento2-${ENVIROMENT} chown -R www-data. /var/www/paylater
+    docker-compose exec -u www-data magento2-${ENVIROMENT} composer install -d /var/www/html/app/code/Digita:lOrigin/Pmt
 else
+    package='dev-master'
     if [ ! -z "$TRAVIS_PULL_REQUEST_BRANCH" ]
     then
         echo "This is the branch of the pull request" ${TRAVIS_PULL_REQUEST_BRANCH}
@@ -42,12 +51,6 @@ else
         echo "This is the branch of the branch:" ${TRAVIS_BRANCH}
         package=${TRAVIS_BRANCH}'.x-dev'
     fi
-    if [ -z "$package" ]
-    then
-        echo "This is the branch master:" ${TRAVIS_TAG}
-        package='dev-master'
-    fi
-
 
     echo 'Running: composer requiere pagamastarde/magento-2x:'$package' -d /var/www/html'
     docker-compose exec -u www-data magento2-${ENVIROMENT} composer require pagamastarde/magento-2x:$package -d /var/www/html
@@ -57,17 +60,12 @@ else
         --clear-static-content
 fi
 
-
-echo 'Sample Data + DI + SetupUpgrade + Clear Cache'
-docker-compose exec -u www-data magento2-${ENVIROMENT} composer config http-basic.repo.magento.com \
-    5310458a34d580de1700dfe826ff19a1 \
-    255059b03eb9d30604d5ef52fca7465d
-echo 'Running: sampledata:deploy'
-docker-compose exec -u www-data magento2-${ENVIROMENT} php /var/www/html/bin/magento sampledata:deploy
-echo 'Running: setup:upgrade'
-docker-compose exec -u www-data magento2-${ENVIROMENT} php /var/www/html/bin/magento setup:upgrade
 echo 'Running: cron:run'
 docker-compose exec -u www-data magento2-${ENVIROMENT} php /var/www/html/bin/magento cron:run
+#echo 'Running: chmod 777 -R generated'
+#docker-compose exec magento2-${ENVIROMENT} chmod 777 -R generated
+#echo 'Running: chmod 777 -R var/cache'
+#docker-compose exec magento2-${ENVIROMENT} chmod 777 -R var/cache
 
 if [ $1 == 'test' ]
 then
