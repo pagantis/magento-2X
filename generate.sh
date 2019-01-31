b2dbb73bc66f542d88314bdc95402e8bfa90ee61
rmarinleal <raul.marin.leal@gmail.com>
2019-01-31 08:11:43 +0100

    traces on generate.sh to se params passed to tests

--- generate.sh
+++ generate.sh
@@ -7,8 +7,13 @@ composer install --ignore-platform-reqs
 set -e
 
 # Run test
+echo "running tests vendor/bin/phpunit --group magento-basic -d magentoVersion -d $1"
 vendor/bin/phpunit --group magento-basic -d magentoVersion -d $1
+echo "running tests vendor/bin/phpunit --group magento-install -d magentoVersion -d $1"
 vendor/bin/phpunit --group magento-install  -d magentoVersion -d $1
+echo "running tests vendor/bin/phpunit --group magento-buy-unregistered -d magentoVersion -d $1"
 vendor/bin/phpunit --group magento-buy-unregistered  -d magentoVersion -d $1
+echo "running tests vendor/bin/phpunit --group magento-register -d magentoVersion -d $1"
 vendor/bin/phpunit --group magento-register  -d magentoVersion -d $1
+echo "running tests vendor/bin/phpunit --group magento-buy-registered -d magentoVersion -d $1"
 vendor/bin/phpunit --group magento-buy-registered  -d magentoVersion -d $1
