a8cb91149aeabf36d2707df94539612c3282162e
rmarinleal <raul.marin.leal@gmail.com>
2019-01-31 08:39:08 +0100

    .travis.yml fixing

--- generate.sh
+++ generate.sh
@@ -10,10 +10,10 @@ set -e
 echo "running tests vendor/bin/phpunit --group magento-basic -d magentoVersion -d $1"
 vendor/bin/phpunit --group magento-basic -d magentoVersion -d $1
 echo "running tests vendor/bin/phpunit --group magento-install -d magentoVersion -d $1"
-vendor/bin/phpunit --group magento-install  -d magentoVersion -d $1
+vendor/bin/phpunit --group magento-install -d magentoVersion -d $1
 echo "running tests vendor/bin/phpunit --group magento-buy-unregistered -d magentoVersion -d $1"
-vendor/bin/phpunit --group magento-buy-unregistered  -d magentoVersion -d $1
+vendor/bin/phpunit --group magento-buy-unregistered -d magentoVersion -d $1
 echo "running tests vendor/bin/phpunit --group magento-register -d magentoVersion -d $1"
-vendor/bin/phpunit --group magento-register  -d magentoVersion -d $1
+vendor/bin/phpunit --group magento-register -d magentoVersion -d $1
 echo "running tests vendor/bin/phpunit --group magento-buy-registered -d magentoVersion -d $1"
-vendor/bin/phpunit --group magento-buy-registered  -d magentoVersion -d $1
+vendor/bin/phpunit --group magento-buy-registered -d magentoVersion -d $1
