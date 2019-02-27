7562b5a59f84ad621d7808542571fb1459cb8848
pgarces <gspablo@gmail.com>
2019-02-27 09:34:26 +0100

    Remove simulator on checkout

--- view/frontend/web/js/view/payment/method-renderer/paylater.js
+++ view/frontend/web/js/view/payment/method-renderer/paylater.js
@@ -7,18 +7,15 @@ define(
         'Magento_Checkout/js/model/error-processor',
         'Magento_Checkout/js/model/full-screen-loader',
         'Magento_Checkout/js/model/quote',
-        '//cdn.pagamastarde.com/pmt-js-client-sdk/3/js/client-sdk.min.js',
         'Magento_Checkout/js/action/select-payment-method',
         'Magento_Checkout/js/checkout-data',
         'Magento_Checkout/js/model/totals',
         'Magento_Catalog/js/price-utils'
     ],
-    function ($, Component, url, customerData, errorProcessor, fullScreenLoader, quote, pmtClient, selectPaymentMethodAction, checkoutData, totals, priceUtils) {
+    function ($, Component, url, customerData, errorProcessor, fullScreenLoader, quote, selectPaymentMethodAction, checkoutData, totals, priceUtils) {
         'use strict';
 
         window.checkoutConfig.payment.paylater.guestEmail = quote.guestEmail;
-
-        window.pmtClient = pmtClient;
 
         return Component.extend({
                 defaults: {
@@ -27,49 +24,12 @@ define(
 
                 redirectAfterPlaceOrder: false,
 
-                loadSimulator: function ()
-                {
-                    setTimeout(function(){
-                        if (window.checkoutConfig.payment.paylater.pmtType  !='0' &&
-                            window.checkoutConfig.payment.paylater.publicKey!=''  &&
-                            window.checkoutConfig.payment.paylater.secretKey!='')
-                        {
-                            if (typeof window.pmtClient !== 'undefined')
-                            {
-                                window.pmtClient.setPublicKey(window.checkoutConfig.payment.paylater.publicKey);
-                                window.pmtClient.simulator.reload();
-                                return true;
-                            }
-                        }
-                    }, 3000);
+                getTitle: function () {
+                    return window.checkoutConfig.payment.paylater.title
                 },
 
                 getSubtitle: function () {
                     return window.checkoutConfig.payment.paylater.subtitle
-                },
-
-                getPmtNumQuota: function () {
-                    return window.checkoutConfig.payment.paylater.pmtNumQuota
-                },
-
-                dataPmtMaxIns: function () {
-                    return window.checkoutConfig.payment.paylater.pmtMaxIns
-                },
-
-                getPmtType: function () {
-                    return window.checkoutConfig.payment.paylater.pmtType
-                },
-
-                getPmtTotal: function () {
-                    return totals.totals().base_grand_total;
-                },
-
-                getPublicKey: function () {
-                    return window.checkoutConfig.payment.paylater.publicKey
-                },
-
-                getSecretKey: function () {
-                    return window.checkoutConfig.payment.paylater.secretKey
                 },
 
                 getDisplayMode: function () {
