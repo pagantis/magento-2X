f87976d9f550c7fd5d77b80126a1c33c0f2b2f6f
pgarces <gspablo@gmail.com>
2019-03-13 12:46:41 +0100

    Change method string

--- Test/PaylaterMagentoTest.php
+++ Test/PaylaterMagentoTest.php
@@ -102,7 +102,7 @@ abstract class PaylaterMagentoTest extends TestCase
         'publicKey'          => 'tk_fd53cd467ba49022e4f8215e',
         'secretKey'          => '21e57baa97459f6a',
         'methodName'         => 'Pagantis',
-        'methodTitle'        => 'Financiación instántanea',
+        'methodTitle'        => 'Financiación instantánea',
         'defaultSimulatorOpt'=> 6,
         'defaultMinIns'      => 3,
         'defaultMaxIns'      => 12,
