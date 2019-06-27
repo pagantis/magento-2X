fc5efd3fe862ea1e11bd1f6ea05922fc5c83da96
pgarces <gspablo@gmail.com>
2019-06-27 15:05:28 +0200

    Set the class of the simulator like pagantisSimulator

--- Test/Common/AbstractMg21Selenium.php
+++ Test/Common/AbstractMg21Selenium.php
@@ -483,7 +483,7 @@ abstract class AbstractMg21Selenium extends PagantisMagentoTest
      */
     private function checkSimulator()
     {
-        $simulatorElementSearch = WebDriverBy::className('installmentsSimulator');
+        $simulatorElementSearch = WebDriverBy::className('pagantisSimulator');
         $condition = WebDriverExpectedCondition::visibilityOfElementLocated($simulatorElementSearch);
         $this->webDriver->wait()->until($condition);
         $this->assertTrue((bool) $condition, "PR19//PR28");
