84761fb54102dad4afa4b586b43a4bdf7ebafb0a
pgarces <gspablo@gmail.com>
2019-08-14 09:04:46 +0200

    Create pagantis_promoted field

--- Setup/UpgradeData.php
+++ Setup/UpgradeData.php
@@ -6,6 +6,7 @@ use Magento\Framework\Setup\UpgradeDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Pagantis\Pagantis\Helper\Config;
+use Magento\Eav\Setup\EavSetupFactory;
 
 class UpgradeData implements UpgradeDataInterface
 {
@@ -15,9 +16,33 @@ class UpgradeData implements UpgradeDataInterface
     /** @var Config */
     public $config;
 
-    public function __construct(Config $config)
+    /** @var string  */
+    public $code;
+
+    /** @var string  */
+    public $group;
+
+    /** @var string  */
+    public $label;
+
+    /**
+     * @var EavSetupFactory
+     */
+    protected $eavSetupFactory;
+
+    /**
+     * UpgradeData constructor.
+     *
+     * @param Config          $config
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(Config $config, EavSetupFactory $eavSetupFactory)
     {
         $this->config = $config;
+        $this->code = 'pagantis_promoted';
+        $this->group = 'General';
+        $this->label = 'Pagantis Promoted';
+        $this->eavSetupFactory = $eavSetupFactory;
     }
 
     /**
@@ -35,6 +60,34 @@ class UpgradeData implements UpgradeDataInterface
                 $setup->getConnection()->insert(self::CONFIG_TABLE, array('config'=>$config, 'value'=>$value));
             }
         }
+
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+        $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Product::ENTITY,
+            $this->code,
+            [
+                'group' => $this->group,
+                'type' => 'int',
+                'backend' => '',
+                'frontend' => '',
+                'label' => $this->label,
+                'input' => 'boolean',
+                'class' => '',
+                'source' => 'Magento\Eav\Model\Entity\Attribute\Source\Boolean',
+                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
+                'visible' => true,
+                'required' => false,
+                'user_defined' => false,
+                'default' => '0',
+                'searchable' => false,
+                'filterable' => false,
+                'comparable' => false,
+                'visible_on_front' => false,
+                'used_in_product_listing' => false,
+                'unique' => false,
+                'apply_to' => ''
+            ]
+        );
 
         $setup->endSetup();
     }
