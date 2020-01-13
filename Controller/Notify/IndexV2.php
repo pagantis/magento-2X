e8c24372207da015c5f392658d9129d88354c39d
pgarces <gspablo@gmail.com>
2020-01-13 22:57:32 +0100

    Move parent call

--- Controller/Notify/IndexV2.php
+++ Controller/Notify/IndexV2.php
@@ -136,7 +136,6 @@ class IndexV2 extends Action
         Session $checkoutSession,
         ExtraConfig $extraConfig
     ) {
-        parent::__construct($context);
         $this->quote = $quote;
         $this->quoteManagement = $quoteManagement;
         $this->paymentInterface = $paymentInterface;
@@ -156,6 +155,8 @@ class IndexV2 extends Action
                 $request->setParam('form_key', $formKey->getFormKey());
             }
         }
+
+        parent::__construct($context);
     }
 
     /**
