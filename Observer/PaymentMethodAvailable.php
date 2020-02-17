889258e571eb0ca54c636b0c78c6202eda44def5
pgarces <gspablo@gmail.com>
2020-02-17 16:05:58 +0100

    Add restriction to max_amount in checkout

--- Observer/PaymentMethodAvailable.php
+++ Observer/PaymentMethodAvailable.php
@@ -18,6 +18,7 @@ class PaymentMethodAvailable implements ObserverInterface
         try {
             if ($observer->getEvent()->getMethodInstance()->getCode()=="pagantis") {
                 $checkResult = $observer->getEvent()->getResult();
+                $totalPrice  = (string) floor($observer->getEvent()->getQuote()->getGrandTotal());
                 $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
                 $config        = $objectManager->create('Pagantis\Pagantis\Helper\Config')->getConfig();
                 $extraConfig   = $objectManager->create('Pagantis\Pagantis\Helper\ExtraConfig')->getExtraConfig();
@@ -25,9 +26,12 @@ class PaymentMethodAvailable implements ObserverInterface
                 $locale = strstr($resolver->getLocale(), '_', true);
                 $allowedCountries = unserialize($extraConfig['PAGANTIS_ALLOWED_COUNTRIES']);
                 $availableCountry = (in_array(strtolower($locale), $allowedCountries));
+                $maxAmount = $extraConfig['PAGANTIS_DISPLAY_MAX_AMOUNT'];
+                $minAmount = $extraConfig['PAGANTIS_DISPLAY_MIN_AMOUNT'];
+                $validAmount = ($totalPrice>=$minAmount && ($totalPrice<=$maxAmount||$maxAmount=='0'));
                 if (!isset($config['pagantis_public_key']) || $config['pagantis_public_key'] == '' ||
                     !isset($config['pagantis_private_key']) || $config['pagantis_private_key'] == '' ||
-                    !$availableCountry ) {
+                    !$availableCountry || !$validAmount) {
                     $checkResult->setData('is_available', false);
                 } else {
                     $checkResult->setData('is_available', true);
